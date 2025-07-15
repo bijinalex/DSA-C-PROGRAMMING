9275d8ab73c4cf3344d6612411b38f541213896c
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2025-07-15 15:54:12 +0530

    Add files via upload

---
+++ sum_poly.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+struct poly {
+    int coeff;
+    int expo;
+};
+
+int main() {
+    struct poly p1[10], p2[10], p3[20];
+    int m, n, i, j, k = 0;
+
+    printf("Enter the number of terms of the first polynomial: ");
+    scanf("%d", &m);
+    for (i = 0; i < m; i++) {
+        printf("Enter coefficient and exponent for term %d: ", i + 1);
+        scanf("%d %d", &p1[i].coeff, &p1[i].expo);
+    }
+
+    printf("Enter the number of terms of the second polynomial: ");
+    scanf("%d", &n);
+    for (j = 0; j < n; j++) {
+        printf("Enter coefficient and exponent for term %d: ", j + 1);
+        scanf("%d %d", &p2[j].coeff, &p2[j].expo);
+    }
+
+    i = 0;
+    j = 0;
+    while (i < m && j < n) {
+        if (p1[i].expo == p2[j].expo) {
+            p3[k].coeff = p1[i].coeff + p2[j].coeff;
+            p3[k].expo = p1[i].expo;
+            i++; j++; k++;
+        } else if (p1[i].expo > p2[j].expo) {
+            p3[k] = p1[i];
+            i++; k++;
+        } else {
+            p3[k] = p2[j];
+            j++; k++;
+        }
+    }
+
+    // Copy remaining terms
+    while (i < m) {
+        p3[k] = p1[i];
+        i++; k++;
+    }
+    while (j < n) {
+        p3[k] = p2[j];
+        j++; k++;
+    }
+
+    // Print result
+    printf("Sum of the polynomials: ");
+    for (i = 0; i < k; i++) {
+        if (p3[i].coeff == 0) continue;
+
+        if (i > 0 && p3[i].coeff > 0)
+            printf(" + ");
+
+        printf("%dx^%d", p3[i].coeff, p3[i].expo);
+    }
+    printf("\n");
+
+    return 0;
+}
