6ebcdbeca12836357d56f59fd15ca6326eaeef69
BIJIN ALEX <bijinalex2028@cs.sjcetpalai.ac.in>
2025-09-06 21:56:07 +0530

    Update infix.c

--- infix.c
+++ infix.c
@@ -5,35 +5,29 @@
 
 char stack[MAX];
 int top = -1;
-
 void push(char c) {
     if (top < MAX - 1) {
         stack[++top] = c;
     }
 }
-
 char pop() {
     if (top >= 0) {
         return stack[top--];
     }
     return '\0';
 }
-
 int precedence(char c) {
     if (c == '+' || c == '-') return 1;
     if (c == '*' || c == '/') return 2;
     if (c == '^') return 3;
     return 0;
 }
-
 int isOperator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
 }
-
 void infixToPostfix(char* infix, char* postfix) {
     int i = 0, j = 0;
     char current;
-
     while ((current = infix[i++]) != '\0') {
         if (isalnum(current)) {
             postfix[j++] = current;
@@ -51,13 +45,11 @@ void infixToPostfix(char* infix, char* postfix) {
             push(current);
         }
     }
-
     while (top >= 0) {
         postfix[j++] = pop();
     }
     postfix[j] = '\0';
 }
-
 int main() {
     char infix[MAX], postfix[MAX];
     printf("Enter infix expression: ");
